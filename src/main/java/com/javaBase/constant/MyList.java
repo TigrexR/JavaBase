a84b9097cc4c1ba799e7bece4ed3963b010f3369
TigrexR <swcdasd@sina.com>
2018-08-29 17:20:58 +0800

    集合

---
+++ src/main/java/com/javaBase/constant/MyList.java
@@ -0,0 +1,10 @@
+package com.javaBase.constant;
+
+import java.util.Collection;
+
+public interface MyList<E> extends Collection<E> {
+
+    @Override
+    boolean isEmpty();
+
+}
---
+++ src/main/java/com/javaBase/domain/User.java
@@ -0,0 +1,20 @@
+package com.javaBase.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain = true)
+public class User {
+
+    Integer id;
+
+    String name;
+
+    Integer age;
+
+}
